396911272ff0284af545a3a1f351cf3951177556
MaLynda Poulsen <poulsenjones@yahoo.com>
2016-06-21 19:29:54 -0600

    x

--- components/second-components.js
+++ components/second-components.js
@@ -6,5 +6,19 @@
             templateUrl: 'components/second-components.html'
         })
 
+$(function(){
+   // See if this is a touch device
+   if ('ontouchstart' in window)
+   {
+      // Set the correct [touchscreen] body class
+      $('portfolio').removeClass('no-touch').addClass('touch');
+     
+      // Add the touch toggle to show text when tapped
+      $('div.boxInner img').click(function(){
+         $(this).closest('.boxInner').toggleClass('touchFocus');
+      });
+   }
+});
+// </script>
 
 }());
